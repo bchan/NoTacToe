168db2b7318a061be7da02b71589750ea74cc1c7
Benjamin Chan <bchan1298@gmail.com>
2018-12-08 18:37:51 -0500

    Added a new launch screen activity

---
+++ app/src/main/java/com/example/benjaminchan/notactoe/LaunchActivity.java
@@ -0,0 +1,43 @@
+package com.example.benjaminchan.notactoe;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.app.Activity;
+import android.content.Intent;
+
+
+public class LaunchActivity extends Activity implements OnClickListener {
+
+    private Button button1;
+    private Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.launchscreen);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.button1:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+}
--- app/src/main/java/com/example/benjaminchan/notactoe/MainActivity.java
+++ app/src/main/java/com/example/benjaminchan/notactoe/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.view.View.OnClickListener;
 import android.view.View;
 import android.graphics.Color;
@@ -23,15 +24,16 @@ import android.content.Context;
 public class MainActivity extends AppCompatActivity implements OnClickListener, SensorEventListener {
 
     private TextView mTextMessage;
-    private Button button1;
-    private Button button2;
-    private Button button3;
-    private Button button4;
-    private Button button5;
-    private Button button6;
-    private Button button7;
-    private Button button8;
-    private Button button9;
+    private ImageButton button1;
+    private ImageButton button2;
+    private ImageButton button3;
+    private ImageButton button4;
+    private ImageButton button5;
+    private ImageButton button6;
+    private ImageButton button7;
+    private ImageButton button8;
+    private ImageButton button9;
+    private Button resetbutton;
 
     // Board
     Board b = new Board();
@@ -39,41 +41,42 @@ public class MainActivity extends AppCompatActivity implements OnClickListener, 
     private SensorManager mSensorManager;
     private Sensor mLight;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+//    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+//            = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-        @Override
-
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+//        @Override
+//
+//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//            switch (item.getItemId()) {
+//                case R.id.navigation_home:
+//                    mTextMessage.setText(R.string.title_home);
+//                    return true;
+//                case R.id.navigation_dashboard:
+//                    mTextMessage.setText(R.string.title_dashboard);
+//                    return true;
+//                case R.id.navigation_notifications:
+//                    mTextMessage.setText(R.string.title_notifications);
+//                    return true;
+//            }
+//            return false;
+//        }
+//    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button1 = (Button) findViewById(R.id.button1);
-        button2 = (Button) findViewById(R.id.button2);
-        button3 = (Button) findViewById(R.id.button3);
-        button4 = (Button) findViewById(R.id.button4);
-        button5 = (Button) findViewById(R.id.button5);
-        button6 = (Button) findViewById(R.id.button6);
-        button7 = (Button) findViewById(R.id.button7);
-        button8 = (Button) findViewById(R.id.button8);
-        button9 = (Button) findViewById(R.id.button9);
+        button1 = (ImageButton) findViewById(R.id.button1);
+        button2 = (ImageButton) findViewById(R.id.button2);
+        button3 = (ImageButton) findViewById(R.id.button3);
+        button4 = (ImageButton) findViewById(R.id.button4);
+        button5 = (ImageButton) findViewById(R.id.button5);
+        button6 = (ImageButton) findViewById(R.id.button6);
+        button7 = (ImageButton) findViewById(R.id.button7);
+        button8 = (ImageButton) findViewById(R.id.button8);
+        button9 = (ImageButton) findViewById(R.id.button9);
+        resetbutton = (Button) findViewById(R.id.resetbutton);
         //visibility
         button1.setBackgroundColor(Color.TRANSPARENT);
         button2.setBackgroundColor(Color.TRANSPARENT);
@@ -84,6 +87,18 @@ public class MainActivity extends AppCompatActivity implements OnClickListener, 
         button7.setBackgroundColor(Color.TRANSPARENT);
         button8.setBackgroundColor(Color.TRANSPARENT);
         button9.setBackgroundColor(Color.TRANSPARENT);
+
+        button1.setImageResource(android.R.color.transparent);
+        button2.setImageResource(android.R.color.transparent);
+        button3.setImageResource(android.R.color.transparent);
+        button4.setImageResource(android.R.color.transparent);
+        button5.setImageResource(android.R.color.transparent);
+        button6.setImageResource(android.R.color.transparent);
+        button7.setImageResource(android.R.color.transparent);
+        button8.setImageResource(android.R.color.transparent);
+        button9.setImageResource(android.R.color.transparent);
+
+
         //listeners:
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
@@ -94,23 +109,50 @@ public class MainActivity extends AppCompatActivity implements OnClickListener, 
         button7.setOnClickListener(this);
         button8.setOnClickListener(this);
         button9.setOnClickListener(this);
+        resetbutton.setOnClickListener(this);
 
-        button1.setText(" ");
-        button2.setText(" ");
-        button3.setText(" ");
-        button4.setText(" ");
-        button5.setText(" ");
-        button6.setText(" ");
-        button7.setText(" ");
-        button8.setText(" ");
-        button9.setText(" ");
+//        button1.setText(" ");
+//        button2.setText(" ");
+//        button3.setText(" ");
+//        button4.setText(" ");
+//        button5.setText(" ");
+//        button6.setText(" ");
+//        button7.setText(" ");
+//        button8.setText(" ");
+//        button9.setText(" ");
 
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+//        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+    }
+
+    private void resetBoard() {
+        b.reset(3);
+        button1.setEnabled(true);
+        button2.setEnabled(true);
+        button3.setEnabled(true);
+        button4.setEnabled(true);
+        button5.setEnabled(true);
+        button6.setEnabled(true);
+        button7.setEnabled(true);
+        button8.setEnabled(true);
+        button9.setEnabled(true);
+
+        button1.setImageResource(android.R.color.transparent);
+        button2.setImageResource(android.R.color.transparent);
+        button3.setImageResource(android.R.color.transparent);
+        button4.setImageResource(android.R.color.transparent);
+        button5.setImageResource(android.R.color.transparent);
+        button6.setImageResource(android.R.color.transparent);
+        button7.setImageResource(android.R.color.transparent);
+        button8.setImageResource(android.R.color.transparent);
+        button9.setImageResource(android.R.color.transparent);
+
+        mTextMessage.setText("Player 1's Turn");
+
     }
 
     @Override
@@ -125,64 +167,87 @@ public class MainActivity extends AppCompatActivity implements OnClickListener, 
         switch (v.getId()) {
             case R.id.button1:
                 b.addX(1, 1);
-                button1.setText("X");
-
+                //button1.setText("X");
+                button1.setImageResource(R.drawable.ximage);
                 break;
             case R.id.button2:
                 b.addX(1, 2);
-                button2.setText("X");
+                //button2.setText("X");
+                button2.setImageResource(R.drawable.ximage);
                 break;
             case R.id.button3:
                 b.addX(1, 3);
-                button3.setText("X");
+                //button3.setText("X");
+                button3.setImageResource(R.drawable.ximage);
                 break;
             case R.id.button4:
                 b.addX(2, 1);
-                button4.setText("X");
+                //button4.setText("X");
+                button4.setImageResource(R.drawable.ximage);
                 break;
             case R.id.button5:
                 b.addX(2, 2);
-                button5.setText("X");
+                //button5.setText("X");
+                button5.setImageResource(R.drawable.ximage);
                 break;
             case R.id.button6:
                 b.addX(2, 3);
-                button6.setText("X");
+                //button6.setText("X");
+                button6.setImageResource(R.drawable.ximage);
                 break;
             case R.id.button7:
                 b.addX(3, 1);
-                button7.setText("X");
+                //button7.setText("X");
+                button7.setImageResource(R.drawable.ximage);
                 break;
             case R.id.button8:
                 b.addX(3, 2);
-                button8.setText("X");
+                //button8.setText("X");
+                button8.setImageResource(R.drawable.ximage);
                 break;
             case R.id.button9:
                 b.addX(3, 3);
-                button9.setText("X");
+                //button9.setText("X");
+                button9.setImageResource(R.drawable.ximage);
+                break;
+            case R.id.resetbutton:
+                resetBoard();
                 break;
         }
 
 
         if(b.checkThree())
         {
-            mTextMessage.setText("You lose! Press middle button to reset.");
-            if (v.getId() == R.id.button5) {
-                mTextMessage.setText("Player 1's Turn");
-                button1.setText(" ");
-                button2.setText(" ");
-                button3.setText(" ");
-                button4.setText(" ");
-                button5.setText(" ");
-                button6.setText(" ");
-                button7.setText(" ");
-                button8.setText(" ");
-                button9.setText(" ");
-                b.reset(3);
-            }
+            mTextMessage.setText("You lose!");
+            button1.setEnabled(false);
+            button2.setEnabled(false);
+            button3.setEnabled(false);
+            button4.setEnabled(false);
+            button5.setEnabled(false);
+            button6.setEnabled(false);
+            button7.setEnabled(false);
+            button8.setEnabled(false);
+            button9.setEnabled(false);
+//            if (v.getId() == R.id.button5) {
+//                mTextMessage.setText("Player 1's Turn");
+//                button1.setText(" ");
+//                button2.setText(" ");
+//                button3.setText(" ");
+//                button4.setText(" ");
+//                button5.setText(" ");
+//                button6.setText(" ");
+//                button7.setText(" ");
+//                button8.setText(" ");
+//                button9.setText(" ");
+//
+//                b.reset(3);
+//            }
         }
 
 
     }
+
+
 
     // From https://developer.android.com/guide/topics/sensors/sensors_environment#java
     @Override
@@ -193,16 +258,17 @@ public class MainActivity extends AppCompatActivity implements OnClickListener, 
     @Override
     public final void onSensorChanged(SensorEvent event) {
         float luxValue = event.values[0];
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.frame);
+        if (luxValue > 100) {
 
-        if (luxValue > 1) {
-            RelativeLayout rl = (RelativeLayout) findViewById(R.id.frame);
             //rl.setBackgroundColor(Color.GRAY);
             rl.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.darkThemeBackground));
+        } else {
+            rl.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.darkThemeText));
         }
 
         //if (luxValue > 100000) {
         //mTextMessage.setText(Float.toString(luxValue));
-        // Do something with this sensor data.
     }
 
     @Override
